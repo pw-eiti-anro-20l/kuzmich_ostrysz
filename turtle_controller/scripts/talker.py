625cd93982dcf1c7dcdfe4fb5657e460698466f2
Jakub <kuba.ostrysz@gmail.com>
2020-03-22 19:50:48 +0100

    Poprawnie dzialajacy wezel

--- turtle_controller/scripts/talker.py
+++ turtle_controller/scripts/talker.py
@@ -5,6 +5,7 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 import sys, select, termios, tty
 
+#funkcja getKey() wczytuje znak z klawiatury ktory nie jest bufforowany przez ENTER
 def getKey():
     fd = sys.stdin.fileno()
     newattr = termios.tcgetattr(fd)
@@ -15,11 +16,14 @@ def getKey():
     termios.tcsetattr(sys.stdin, termios.TCSANOW, newattr)
     reset()
     return key
- 
+
+#funkcja reset() resetuje terminal do domyslnych ustawien
 def reset():
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, termios.tcgetattr(sys.stdin))
 	termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, termios.tcgetattr(sys.stdin.fileno()))
-	
+
+#funkcja talker() inicjuje komunikacje pomiedzy wezlami i pozwala na poruszanie zolwiem klawiszami "w" "s" "a" "d" 
+# ktore sa wczytywane z sewera parametrow
 def talker():
     pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=1)
     rospy.init_node('talker', anonymous=True)
@@ -27,16 +31,16 @@ def talker():
     twist = Twist()
     while not rospy.is_shutdown():
         if getKey() == rospy.get_param("/forward"):
-            twist.linear.x = 2
+            twist.linear.x = 1
 	    twist.angular.z = 0
         elif getKey() == rospy.get_param("/backward"):
-            twist.linear.x = -2
+            twist.linear.x = -1
 	    twist.angular.z = 0
         elif getKey() == rospy.get_param("/left"):
-            twist.angular.z = 2
+            twist.angular.z = 1
             twist.linear.x = 0
         elif getKey() == rospy.get_param("/right"):
-            twist.angular.z = -2
+            twist.angular.z = -1
             twist.linear.x = 0
         elif getKey() == "\x03":
             raise KeyboardInterrupt
