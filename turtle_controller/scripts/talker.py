0d5c33a43e78d5d5b6d3eba29cb6b0f499dc9897
Jakub <kuba.ostrysz@gmail.com>
2020-03-22 19:11:23 +0100

    Poprawnie dzialajaca wersja (nieopisana)

--- turtle_controller/scripts/talker.py
+++ turtle_controller/scripts/talker.py
@@ -6,39 +6,52 @@ from geometry_msgs.msg import Twist
 import sys, select, termios, tty
 
 def getKey():
+    fd = sys.stdin.fileno()
+    newattr = termios.tcgetattr(fd)
+    newattr[3] &= ~termios.ICANON
+    newattr[3] &= ~termios.ECHO
     tty.setraw(sys.stdin.fileno())
-    select.select([sys.stdin], [], [], 0)
     key = sys.stdin.read(1)
-    termios.tcsetattr(sys.stdin, termios.TCSANOW, termios.tcgetattr(sys.stdin.fileno()))
+    termios.tcsetattr(sys.stdin, termios.TCSANOW, newattr)
+    reset()
     return key
-
+ 
+def reset():
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, termios.tcgetattr(sys.stdin))
+	termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, termios.tcgetattr(sys.stdin.fileno()))
+	
 def talker():
-    pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+    pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=1)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(100) #10Hz
+    rate = rospy.Rate(10) #10Hz
     twist = Twist()
     while not rospy.is_shutdown():
         if getKey() == rospy.get_param("/forward"):
-            twist.linear.x = 1
+            twist.linear.x = 2
 	    twist.angular.z = 0
         elif getKey() == rospy.get_param("/backward"):
-            twist.linear.x = -1
+            twist.linear.x = -2
 	    twist.angular.z = 0
         elif getKey() == rospy.get_param("/left"):
-            twist.angular.z = 1
+            twist.angular.z = 2
             twist.linear.x = 0
         elif getKey() == rospy.get_param("/right"):
-            twist.angular.z = -1
+            twist.angular.z = -2
             twist.linear.x = 0
         elif getKey() == "\x03":
-            rospy.signal_shutdown("Pressed CTRL-c. Killing node.")
+            raise KeyboardInterrupt
 	else:
             twist.linear.x = 0
             twist.angular.z = 0
+	rospy.loginfo(twist)
 	pub.publish(twist)
+	rate.sleep()	
 
 if __name__ == '__main__':
     try:
+	newattr = termios.tcgetattr(sys.stdin)
         talker()
     except rospy.ROSInterruptException:
         pass
+    finally:
+	reset()
